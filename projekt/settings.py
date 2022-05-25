632eb8994aaaa8b1ed095b420f0f4277c1a390f2
Teodora <teodoramaros@ssklivno.net>
2022-05-25 09:25:17 +0200

    changed something to fix our error

--- projekt/settings.py
+++ projekt/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'myproject.urls'
+ROOT_URLCONF = 'projekt.urls'
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'myproject.wsgi.application'
+WSGI_APPLICATION = 'projekt.wsgi.application'
 
 
 # Database
